628387a06705a643d8570d6427effe6dfb88bca6
VanGog06 <vangog064@gmail.com>
2016-04-15 22:28:10 +0300

    Hand.java

---
+++ Hand.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Hand {
+    private List<Card> hand;
+
+    public Hand() {
+        this.hand = new ArrayList<>();
+    }
+
+    public List<Card> getHand() {
+        return this.hand;
+    }
+
+    public void populateHand(Deck d, List<Card> deck) {
+        Random rnd = new Random();
+
+        for (int i = 0; i < 5; i++) {
+            Card card = d.getDeck().get(rnd.nextInt(deck.size()));
+            hand.add(card);
+            d.removeCardFromDeck(deck, card);
+        }
+    }
+
+    public void swapCard(List<Card> hand, Deck d, int index) {
+        Random rnd = new Random();
+        Card c = d.getDeck().get(rnd.nextInt(d.getDeck().size()));
+
+        hand.remove(index);
+        d.removeCardFromDeck(d.getDeck(), c);
+        hand.add(index, c);
+    }
+}
