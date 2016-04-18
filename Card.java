c16e24185591b9cd937c0b2ea8f095a02f138e9f
VanGog06 <vangog06406@gmail.com>
2016-04-18 17:50:46 +0300

    Added Comparable implementation

--- Card.java
+++ Card.java
@@ -1,4 +1,4 @@
-public class Card {
+public class Card implements Comparable<Card> {
 
     private static String[] ranks =
             { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
@@ -6,19 +6,24 @@ public class Card {
     private int rank;
     private int suit;
 
-    public Card (int rank, int suit){
+    public Card(int rank, int suit) {
         this.rank = rank;
         this.suit = suit;
     }
 
-    public int getRank(){
+    public int getRank() {
         return rank;
     }
-    public int getSuit(){
+    public int getSuit() {
         return suit;
     }
 
-    public String toString(){
+    public String toString() {
         return ranks[rank] + " " + suits[suit];
+    }
+
+    @Override
+    public int compareTo(Card otherCard) {
+        return ranks[this.rank].compareTo(ranks[otherCard.getRank()]);
     }
 }
