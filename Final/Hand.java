a37fab2bc895b40a272c9591bcab7fd1155f274b
VanGog06 <vangog06406@gmail.com>
2016-04-21 10:56:43 +0300

    Update Hand.java

--- Final/Hand.java
+++ Final/Hand.java
@@ -11,6 +11,7 @@ public class Hand {
         this.hand = new ArrayList<>();
     }
 
+    //populate the cards in the hand
     public List<Card> populateHand(Deck d, List<Card> deck) {
         Random rnd = new Random();
 
@@ -23,15 +24,18 @@ public class Hand {
         return hand;
     }
 
+    //return the current cards in the hand
     public List<Card> getHand() {
         return this.hand;
     }
 
+    //remove the card that you don't want to hold and replace it with a new one
     public void swapCardInHand(List<Card> hand, Deck d, int[] indexes) {
         Random rnd = new Random();
 
         for (int i = 0; i < indexes.length; i++) {
             if (indexes[i] > 0) {
+                //generate a random card from the deck
                 Card card = d.getDeck().get(rnd.nextInt(d.getDeck().size()));
                 hand.remove(indexes[i] - 1);
                 hand.add(indexes[i] - 1, card);
